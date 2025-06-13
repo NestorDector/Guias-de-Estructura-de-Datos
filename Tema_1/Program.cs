d7023fd8268f527647b68709982b4bd8fe0ca77b
Nestor Dector <dectorn56@outlook.com>
2025-06-13 15:54:19 -0600

    Commit tema 1

---
+++ Tema_1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JerarquiaDatos
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Ciudad { get; set; }
+
+        public Persona(string nombre, int edad, string ciudad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Ciudad = ciudad;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine($"Nombre: {Nombre,-15} | Edad: {Edad,3} | Ciudad: {Ciudad}");
+            // {cadena,numero} el numero indica cuantos espacios voy a reservar
+            // un numero positivo reserva x espacios a la derecha para la cadena
+            // Un numero negativo reserva x espacios a la izquierda para la cadena
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> personas = new List<Persona>();
+
+            Console.WriteLine("=== Registro de Personas ===");
+
+            while (true)
+            {
+                Console.WriteLine("Ingrese el nombre (o escriba \"fin\" para terminar)");
+                string nombre = Console.ReadLine();
+                if (nombre.ToLower() == "fin")
+                {
+                    break;
+                }
+
+                int edad;
+                Console.WriteLine("Ingrese la edad: ");
+                while (!int.TryParse(Console.ReadLine(), out edad) || edad < 0)
+                {
+                    Console.WriteLine("Ingrese una edad válida");
+                }
+
+                Console.WriteLine("Ingrese la ciudad: ");
+                string ciudad = Console.ReadLine();
+
+                personas.Add(new Persona(edad: edad, nombre: nombre, ciudad: ciudad));
+                Console.WriteLine(">> Persona registrada \n");
+
+                Console.WriteLine("\n=== Lista de Personas Ingresadas ===");
+                foreach (var persona in personas)
+                {
+                    persona.Mostrar();
+                }
+
+            }
+        }
+    }
+}
---
+++ guia.md
@@ -0,0 +1,37 @@
+# Guía del primer parcial - Estructura de Datos
+
+## Tema 1: Jerarquía de Datos
+
+### Archivos/Ficheros
+- Un archivo es una unidad de almacenamiento que contiene datos estructurados en registros.
+- Es un conjunto de datos estructurados en una colección de entidades elementales denominadas *registros* o *artículos* que son de igual tipo y constan de diferentes entidades de nivel más bajo denominadas *campos*.
+- Permiten guardar información de manera permanente.
+
+### Estructura jerárquica de la información
+1. Bit: unidad mínima (0,1)
+2. Caracter: Letra, número o símbolo
+3. Campo: Grupo de caracteres con significado
+4. Registro: Conjunto de campos relacionados
+5. Archivo: Conjunto de registros
+6. Base de datos: Conjunto de archivos relacionados
+
+### Campo
+- Un campo es la unidad mínima de información con significado.
+- Tiene un tipo de dato y una longitud
+- Puede dividirse en subcampos
+
+### Registro
+- Un registro es un conjunto de campos relacionados entre sí.
+- Representa una entidad completa.
+- Puede tener longitud fija o variable.
+- Equivale a una estructura de datos en programación.
+
+### Archivo
+- Es un conjunto organizado de registros similares
+- Se utiliza para almacenar y procesar datos con un propósito definido
+
+### Base de Datos
+- Es un conjunto de archivos relacionados entre sí que son accesible por programas.
+- Permiten centralizar la información.
+
+### Programa
